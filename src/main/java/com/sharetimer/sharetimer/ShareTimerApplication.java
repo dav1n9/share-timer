15647cedd3d86ead4a5e7851675cbdf7c274586b
Davin <85967571+dav1n9@users.noreply.github.com>
2024-03-30 21:21:57 +0900

    initial commit

---
+++ src/main/java/com/sharetimer/sharetimer/ShareTimerApplication.java
@@ -0,0 +1,13 @@
+package com.sharetimer.sharetimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareTimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharetimer/sharetimer/ShareTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharetimer.sharetimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
